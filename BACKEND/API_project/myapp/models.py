43cf6245c77faa726cb04d5a868d4da29d25b250
shaikh farukh <shaikhfarukh60@gmail.com>
2025-08-24 16:34:58 +0530

    model

--- BACKEND/API_project/myapp/models.py
+++ BACKEND/API_project/myapp/models.py
@@ -32,4 +32,17 @@ class Cart(models.Model):
     def __str__(self):
         return self.user.username
 
+class Order(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10,decimal_places=2)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Oder_item(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantaty = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+
+
     
