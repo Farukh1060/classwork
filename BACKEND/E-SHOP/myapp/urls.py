e8058b6cff8901f33e15f6fa16d63d769530b804
shaikh farukh <shaikhfarukh60@gmail.com>
2025-09-01 21:41:33 +0530

    eshop-frontend

--- BACKEND/E-SHOP/myapp/urls.py
+++ BACKEND/E-SHOP/myapp/urls.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from myapp.views import *
 
 urlpatterns = [
-    path("",index,name="index"),
-    # path("",home,name="home"),
+    path("",home,name="home"),
+    path("shop/",shop,name="shop"),
+    path("account/",account,name="account"),
+    path("logout_user/",logout_user,name="logout-user"),
+    path("login_user/",login_user ,name="login_user"),
+    path("register_user/",register_user,name="register_user"),
+    path("login-register/",login_register,name="login-register"),
+
+    path("cart/",cart,name="cart")
+
+
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
--- BACKEND/E-SHOP/myapp/views.py
+++ BACKEND/E-SHOP/myapp/views.py
@@ -3,9 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+def home(request):
     return render(request, "index.html")
 
 
-# def index(request):
-#     return render(request, "base.html")
+def shop(request):
+    return render(request, "shop.html")
+
+def account(request):
+    return render(request,"accounts.html")
+
+def logout_user(request):
+    return render(request,"login-register.html")
+
+def login_user(request):
+    return render(request,"login-register.html")
+
+def register_user(request):
+    return render(request,"login-register.html")
+
+def login_register(request):
+    return render(request,"login-register.html")
+
+def cart(request):
+    return render(request,"cart.html")
