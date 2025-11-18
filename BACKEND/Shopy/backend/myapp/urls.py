07156dae90c2857a9052642b7de91d383c4911e4
shaikh farukh <shaikhfarukh60@gmail.com>
2025-11-18 20:50:58 +0530

    change backend

--- BACKEND/Shopy/backend/myapp/urls.py
+++ BACKEND/Shopy/backend/myapp/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path("api/verify-payment/", VerifyRazorpayPayment.as_view(), name="verify-payment"),
 
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT
-)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT
+    )
